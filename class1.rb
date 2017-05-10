222298fb54c3d5d6ac509547e71a367e07f413e1
ArcangelM <saio8812@hotmail.com>
2017-05-09 19:28:55 -0500

    archivo class 1 actualizado

---
+++ class1.rb
@@ -0,0 +1,16 @@
+#puts 1,'hola clase',1.0
+puts 'ingresa por favor tu nombre:'
+nombre = gets.chomp
+
+puts 'ingresa por favor tu edad:'
+edad = gets.chomp
+puts 'tu edad es:',edad
+puts 'hola como estas '+nombre+' tienes '+edad+' años de edad'
+
+numero1=50
+numero2=100
+
+resultado= numero1+numero2
+promedio= resultado/2
+
+puts promedio +"promedio"
